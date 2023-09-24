06cac34b428db10ae493ea6fee37f7a1fac2aeb6
LINHTK\tklin <tklinh2601@gmail.com>
2023-09-24 21:49:55 +0700

    fix: entity

---
+++ AcademicBlog.Infrastructure/Entities/Bookmark.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AcademicBlog.Infrastructure.Entities;
+
+public partial class Bookmark
+{
+    public int Id { get; set; }
+
+    public int PostId { get; set; }
+
+    public int CreatorId { get; set; }
+
+    public virtual Account Creator { get; set; } = null!;
+
+    public virtual Post Post { get; set; } = null!;
+}
