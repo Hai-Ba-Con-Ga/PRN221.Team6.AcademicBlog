85ec03ba4d4b6fbd3c997db3d1470855f68e0598
YatinCyber <chabonglatui@gmail.com>
2023-10-28 12:31:20 +0700

    (+) add profile page

---
+++ AcademicBlog/Profile/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AcademicBlog.Profile
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
